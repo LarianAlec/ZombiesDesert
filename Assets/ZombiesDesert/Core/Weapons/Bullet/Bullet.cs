3b1277ba167c21a575464b3762334215854b8c10
LarianAlec <lay9997@mail.ru>
2024-03-22 21:49:39 +0300

    Bullet impact VFX

--- Assets/ZombiesDesert/Core/Weapons/Bullet/Bullet.cs
+++ Assets/ZombiesDesert/Core/Weapons/Bullet/Bullet.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject hitVFX;
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contact = collision.contacts[0];
+            GameObject impactFX = Instantiate(hitVFX, contact.point, Quaternion.LookRotation(contact.normal));
+            Destroy(impactFX, 1.0f);
+        }
+        
         Destroy(gameObject);
     }
 }
